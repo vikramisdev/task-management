170db546876605d9c89b81fb691ff0b27097aac0
Vikram <vs423502@gmail.com>
2024-11-17 18:52:17 +0530

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(HttpClientModule)] // Add HttpClientModule here
+}).catch((err) => console.error(err));
